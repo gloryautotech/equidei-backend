ee030038242860df22b0352dcc6fabb7d5f7c7a1
pramit-polynomial <pramit@polynomial.ai>
2023-03-17 22:48:14 +0530

    pinata api changes

--- controller/pinata.js
+++ controller/pinata.js
@@ -6,7 +6,10 @@ const CryptoJS = require('crypto-js');
 const JWT = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4OTM4NjY5Zi04NjI2LTRmMjQtOWI3OC1jY2U3ZWMyNDg1OGIiLCJlbWFpbCI6InByYW1pdEBwb2x5bm9taWFsLmFpIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImVkYTYxYmU5OGFlZmJmZmJmNzU3Iiwic2NvcGVkS2V5U2VjcmV0IjoiZDlhMzhkYjk5M2U2ZWQzOGM0YTE4NDA1MzI2YmI5NDdhOTA5MDQyNDQ3MjY4N2U5NDg3MWVjMjE0NTRlNTBjZCIsImlhdCI6MTY3NzQ5MDk0M30.sL7saKasIkSnt8YP8Ez-JMNbX1Ix_AH4zCB5hdXiUkk'
 const { constants, messages } = require("../constants.js");
 const response = require('../lib/responseLib');
-const userModel = require("../controller/auth")
+const userModel = require("../model/user")
+const assetModel = require("../model/asset")
+let objTemplate = require("../contract.json")
+let { jsPDF } = require("jspdf")
 
 const pinataPinning = async function (req, res) {
     try {
@@ -40,6 +43,7 @@ const pinataPinning = async function (req, res) {
             }
         });
         await fs.unlink('./pramit.txt')
+
         let apiResponse = response.generate(constants.SUCCESS, messages.PINATA.SUCCESS, constants.HTTP_CREATED, resIpfs.data.IpfsHash);
         res.send(apiResponse)
     } catch (err) {
@@ -90,5 +94,99 @@ const fetchIpfsFile = async function (req, res) {
     }
 }
 
+const approve = async function (req, res) {
+    try {
+        const doc = new jsPDF();
+        let asset = await assetModel.findOne({ _id: req.params.assetId })
+        let array = ["jhihuyftdsfrghhj", "yutrsedfghjk", "lkjhuiytrsedfxgcjh"]
+        if (asset == "plantAndMachinary") {
+            if (asset.purchaseBill.IpfsHash) {
+                array.push(asset.purchaseBill.IpfsHash)
+            } else if (asset.taxInvoice.IpfsHash) {
+                array.push(asset.taxInvoice.IpfsHash)
+            } else if (asset.insuranceDoc.IpfsHash) {
+                array.push(asset.insuranceDoc.IpfsHash)
+            } else if (asset.fixedAssetRegister.IpfsHash) {
+                array.push(asset.fixedAssetRegister.IpfsHash)
+            } else if (asset.oldValuationReport.IpfsHash) {
+                array.push(asset.oldValuationReport.IpfsHash)
+            } else if (asset.chargesPending.IpfsHash) {
+                array.push(asset.chargesPending.IpfsHash)
+            } else if (asset.assetInvoice.IpfsHash) {
+                array.push(asset.assetInvoice.IpfsHash)
+            } else if (asset.technicalSpecifications.IpfsHash) {
+                array.push(asset.technicalSpecifications.IpfsHash)
+            }
+        } else {
+            if (asset.propertyTax.IpfsHash) {
+                array.push(asset.propertyTax.IpfsHash)
+            } else if (asset.insuranceDoc.IpfsHash) {
+                array.push(asset.insuranceDoc.IpfsHash)
+            } else if (asset.powerOfAttorney.IpfsHash) {
+                array.push(asset.powerOfAttorney.IpfsHash)
+            } else if (asset.invoice.IpfsHash) {
+                array.push(asset.invoice.IpfsHash)
+            } else if (asset.clearanceCertificate.IpfsHash) {
+                array.push(asset.clearanceCertificate.IpfsHash)
+            } else if (asset.fixedAssetRegister.IpfsHash) {
+                array.push(asset.fixedAssetRegister.IpfsHash)
+            } else if (asset.oldValuationReport.IpfsHash) {
+                array.push(asset.oldValuationReport.IpfsHash)
+            } else if (asset.pendingCharges.IpfsHash) {
+                array.push(asset.pendingCharges.IpfsHash)
+            }
+        }
+        // let findUser = await userModel.findOne({ _id: asset.userId })
+        // let aadhar = findUser.aadhar.aadharNumber
+        // let pan = findUser.PAN.panNumber
+        // let name = findUser.name
+        let objForAddData = {
+            name: "pramit",
+            aadhar: 123434,
+            pan: 1234567,
+            array: array,
+            trId: "pr123456789"
+        }
 
-module.exports = { pinataPinning, fetchIpfsFile }
+
+        let padding = 10;
+        const templateParser = (template, companyDetails) => {
+            for (let key in companyDetails) {
+                template = template.replace(`{${key}}`, companyDetails[key]);
+            }
+            return template;
+        };
+
+        for (let key in objTemplate) {
+            if (key == "heading") {
+                doc.setFontSize(20);
+                // Calculate x and y positions to center the heading
+                var pageWidth = doc.internal.pageSize.width;
+                var textWidth = doc.getStringUnitWidth(objTemplate[key].value) * doc.internal.getFontSize() / doc.internal.scaleFactor;
+                var xPos = (pageWidth - textWidth) / 2;
+                var yPos = 30; // Adjust as needed
+                doc.text(objTemplate[key].value, xPos, yPos);
+            } else if (key == "pararaph") {
+                let contentWidth = doc.internal.pageSize.width - (padding * 2);
+                let contentHeight = doc.internal.pageSize.height - (padding * 2);
+                let contentX = padding;
+                doc.setFontSize(15)
+                let parsedTemplate = templateParser(objTemplate[key].value, objForAddData);
+                doc.text(parsedTemplate, contentX, 50, { maxWidth: 190 }, { width: contentWidth, height: contentHeight });
+            }
+        }
+
+        doc.save("test.pdf");
+
+    } catch (err) {
+        let apiResponse = response.generate(
+            constants.ERROR,
+            messages.udhyam.serverError,
+            constants.HTTP_SERVER_ERROR,
+            err
+        );
+        res.status(500).send(apiResponse);
+    }
+}
+
+module.exports = { pinataPinning, fetchIpfsFile, approve }
